8274f0a65176f5125c6d9ac7d450daeb2e8da565
Corentin Mas <corentinmas@yahoo.com>
2021-04-01 15:51:11 +0200

    [Added] Semi-Functional String printing, prints whole buffer, need to filter it with rax to prevent huge dumps

--- src/print.c
+++ src/print.c
@@ -14,21 +14,33 @@
 #include <stdlib.h>
 #include <string.h>
 
-void print_buffer(int child, unsigned long long addr, unsigned long long len)
+ void print_buffer(int child, unsigned long long addr, int continuous)
 {
+    char *buffer = calloc(1, 1024);
+    unsigned long long int temp = 0;
+    unsigned long int i = 0;
 
-    //  && (!len && buffer != 0)
-    printf("\"");
-    for (int i = 0; (i < len); ++i) {
-        char buffer = (char) ptrace(PTRACE_PEEKDATA, child, addr + i, NULL);
-        printf("%c", buffer);
-        ++i;
+    for (; i <= 1024;) {
+        temp = ptrace(PTRACE_PEEKDATA, child, addr + i, NULL);
+        int l = 0;
+        for (char *t = &temp; l < sizeof(temp) && *t != '\0'; ++t, ++i, ++l)
+            memcpy(&buffer[i], t, 1);
+        if (l < sizeof(temp))
+            break;
     }
-    printf("\"");
+
+    if (!continuous)
+        printf("\"");
+    printf("%s", buffer);
+    free(buffer);
+    if (i >= 1024)
+        return print_buffer(child, addr + i, 1);
+    else
+        printf("\"");
 }
 
-void print_arg(int pid, enum types type, unsigned long long int arg,
-    unsigned long long int next_arg
+
+void print_arg(int pid, enum types type, unsigned long long int arg
 )
 {
     switch (type) {
@@ -39,7 +51,7 @@ void print_arg(int pid, enum types type, unsigned long long int arg,
         printf("0x%llx", arg);
         break;
     case STRING:
-//        print_buffer(pid, arg, next_arg);
+        print_buffer(pid, arg, 0); // todo use rax to get size ?
         break;
     default:
         break;
@@ -55,9 +67,9 @@ void print_syscall(int pid, syscall_t syscall, struct user_regs_struct regs)
     for (int i = 0; i < syscall.nbr_args; ++i) {
         if (i > 0)
             printf(", ");
-        print_arg(pid, syscall.arg_type[i], args[i], args[i + 1]);
+        print_arg(pid, syscall.arg_type[i], args[i]);
     }
     printf(") = ");
-    print_arg(pid, syscall.return_type, regs.rax, 0);
+    print_arg(pid, syscall.return_type, regs.rax);
     printf("\n");
 }
